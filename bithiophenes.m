4d54149ff92378c3d07fda04e5782dc34321aca8
Kate <kpenrod@ycp.edu>
2013-06-03 13:27:48 -0400

    second commit

--- bithiophenes.m
+++ bithiophenes.m
@@ -1,7 +1,7 @@
-cur = 1;
-files = char('bithio1.mat', 'bithio2.mat', 'bithio3.mat');
+cur = 1; %position in for loop
+files = char('bithio1.mat', 'bithio2.mat', 'bithio3.mat'); %change as needed to fit different files
 for cur = 1:size(files,1)
-ldfile = files(cur,:);
+ldfile = files(cur,:); %variable that determines which file is loaded
 load(ldfile);
    spectra(xdata,ydata);
    cur=cur+1;
--- spectra.m
+++ spectra.m
@@ -1,7 +1,8 @@
 %GUI to obtain start values for spectral data fitting
 function spectra(xdata,ydata)
    %gets data of the spectrum from .mat file
-   %will need to find a way of importing actual spectra
+   %will need to create .mat file for each spectrum
+   %each .mat file will contain two variables (xdata,ydata)
    fig = figure;
    plot(xdata, ydata, 'k.-', 'linewidth', 2);
    xlabel('Wavelength(nm)');
@@ -12,21 +13,22 @@ function spectra(xdata,ydata)
    dcm_obj = datacursormode(fig);
    set(dcm_obj,'DisplayStyle','datatip','SnapToDataVertex','on','Enable','on','UpdateFcn',@myupdatefcn);
    hb = uicontrol('style','text');
-   set(hb,'position',[350,520,200,30],'string','For each peak: Select top, then right, then left.');
-   hc = uicontrol('style','togglebutton');
+   set(hb,'position',[350,520,200,30],'string','For each peak: Select top, then right limit, then left limit.');
+   hc = uicontrol('style','pushbutton');
    set(hc,'position',[450,500,100,20],'string','I am finished','callback',@readout);
-   hd = uicontrol('style','togglebutton');
+   hd = uicontrol('style','pushbutton');
    set(hd,'position',[350,500,100,20],'string','Start over','callback',@resetdata);
    function txt = myupdatefcn(~,gcf)
-      % Customizes text of data tips
+      %customizes text of data tip
+      %stores position of data tip to variable
       pos = get(gcf,'Position');
       storedata(end+1,:) = pos;
       txt = {['Wavelength: ',num2str(pos(1))],...
          ['Amplitude: ',num2str(pos(2))]};
    end
    function readout(cur,~,~)
-      %displays data for Gauss1
-      if get(hc,'value')
+      %displays data sent to Gauss1
+      %setting number of gaussians and creating start data for lsqnonlin
          if length(storedata) == 3
             start = [storedata(1,2) storedata(1,1) (storedata(2,1)-storedata(3,1))/2];
             disp(start);
@@ -47,33 +49,35 @@ function spectra(xdata,ydata)
             disp(start);
             f2 = Gauss1(xdata, ydata);
             f2.ngaussians = 4;
-            elseif length(storedata) == 15
+         elseif length(storedata) == 15
             start = [storedata(1,2) storedata(1,1) (storedata(2,1)-storedata(3,1))/2 storedata(4,2) storedata(4,1) (storedata(5,1)-storedata(6,1))/2 storedata(7,2) storedata(7,1) (storedata(8,1)-storedata(9,1))/2 storedata(10,2) storedata(10,1) (storedata(11,1)-storedata(12,1))/2 storedata(13,2) storedata(13,1) (storedata(14,1)-storedata(15,1))/2];
             disp(start);
             f2 = Gauss1(xdata, ydata);
             f2.ngaussians = 5;
-            elseif length(storedata) == 18
+         elseif length(storedata) == 18
             start = [storedata(1,2) storedata(1,1) (storedata(2,1)-storedata(3,1))/2 storedata(4,2) storedata(4,1) (storedata(5,1)-storedata(6,1))/2 storedata(7,2) storedata(7,1) (storedata(8,1)-storedata(9,1))/2 storedata(10,2) storedata(10,1) (storedata(11,1)-storedata(12,1))/2 storedata(13,2) storedata(13,1) (storedata(14,1)-storedata(15,1))/2 storedata(16,2) storedata(16,1) (storedata(17,1)-storedata(18,1))/2];
             disp(start);
             f2 = Gauss1(xdata, ydata);
             f2.ngaussians = 6;
-         end   
-      end
-[pfit2, resnorm, ~, ~, output] = lsqnonlin(@f2.err,start);
-            ypred = f2.ypred(pfit2);
-            hold on;
-            plot (xdata, ydata,'k.',xdata,ypred,'m:','linewidth',3);
-            grid on;
-            set(gcf,'position',[300,50,600,600]);
-            line1 = ['res norm = ',num2str(resnorm)];
-            line8 = output;
-            disp(line1);
-            if length(storedata) == 3
+         end
+      %running lsqnonlin
+      [pfit2, resnorm, ~, ~, output] = lsqnonlin(@f2.err,start);
+      ypred = f2.ypred(pfit2);
+      hold on;
+      plot (xdata, ydata,'k.',xdata,ypred,'m:','linewidth',3);
+      grid on;
+      set(gcf,'position',[300,50,600,600]);
+      line1 = ['res norm = ',num2str(resnorm)];
+      line8 = output;
+      disp(line1);
+      %writing results to file and customizing results for number of
+      %gaussians
+         if length(storedata) == 3
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             disp(line2); 
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             elseif length(storedata) == 6
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             line3 = ['second peak = ',num2str(pfit2(4:6))];
@@ -81,7 +85,7 @@ function spectra(xdata,ydata)
             disp(line3);
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, line3, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             elseif length(storedata) == 9
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             line3 = ['second peak = ',num2str(pfit2(4:6))];
@@ -91,7 +95,7 @@ function spectra(xdata,ydata)
             disp(line4);
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, line3, line4, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             elseif length(storedata) ==12
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             line3 = ['second peak = ',num2str(pfit2(4:6))];
@@ -103,7 +107,7 @@ function spectra(xdata,ydata)
             disp(line5);
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, line3, line4, line5, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             elseif length(storedata) ==15
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             line3 = ['second peak = ',num2str(pfit2(4:6))];
@@ -117,7 +121,7 @@ function spectra(xdata,ydata)
             disp(line6);
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, line3, line4, line5, line6, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             elseif length(storedata) ==18
             line2 = ['first peak  = ',num2str(pfit2(1:3))];
             line3 = ['second peak = ',num2str(pfit2(4:6))];
@@ -133,13 +137,11 @@ function spectra(xdata,ydata)
             disp(line7);
             fileID = fopen('results.txt', 'a+');
             fprintf(fileID, '%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s %s\n %s %s\n %s %s\n %s %s\n %s %s\n', 'New Result', line1, line2, line3, line4, line5, line6, line7, 'output.firstorderopt = ', output.firstorderopt, 'output.iterations = ', output.iterations, 'output.funcCount = ', output.funcCount, 'output.algorithm = ', output.algorithm, 'output.message = ', output.message);
-                fclose(fileID);
+            fclose(fileID);
             end
             disp(line8);
    end
    function resetdata(~,~)
-      if get(hd,'value')
-         storedata = [];
-      end
+      storedata = [];
    end
 end
